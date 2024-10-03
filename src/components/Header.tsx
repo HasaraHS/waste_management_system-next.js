1edf9cee41a83ba5e3b6c7ae424d7086f2b8d73e
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-10-03 17:25:01 +0530

    import lucide-react for icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,6 @@
 'use client'
+import { useState, useEffect } from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { Button } from "./ui/button"
+import {Menu, HandCoins, Leaf, Bell, User, ChevronDown, LogIn, LogOut} from 'lucide-react'
