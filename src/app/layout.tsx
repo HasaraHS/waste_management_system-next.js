01ae42fd53115664b08443902fabe00a31ce7c66
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-10-03 14:17:50 +0530

    install react-hot-toast for toast message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
 'use client'
+import {useState, useEffect} from 'react';
+import {Inter} from 'next/font/google'; //import font 
+import './globals.css';
 
+//header
+//sidebar
+
+import {Toaster} from 'react-hot-toast' //for toast message
 
