5c0ef6130266a41e0ea11ad0bc5af2a3a89bbb3d
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-10-17 11:28:00 +0530

    add google-ai , react-google-map

---
+++ src/app/Report/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { useState, useCallback, useEffect } from "react";
+import {MapPin, Upload, CheckCircle, Loader} from 'lucide-react';
+import {Button} from '@/components/ui/button';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import {StandaloneSearchBox, useJsApiLoader} from '@react-google-maps/api';
+import { Libraries } from "@react-google-maps/api";
+import { useRouter } from "next/navigation";
+import {toast} from 'react-hot-toast';
