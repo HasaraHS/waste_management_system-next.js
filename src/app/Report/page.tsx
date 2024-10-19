e692ac889ac7d42ae5faaae347c4a15d641f7955
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-10-19 12:01:35 +0530

    commit

--- src/app/Report/page.tsx
+++ src/app/Report/page.tsx
@@ -215,4 +215,10 @@ export default function ReportPage() {
 
         checkUser();
     }, [router]);
+
+    return(
+        <div className="p-8 max-w-4xl mx-auto">
+            <h1 className="text-3xl font-semibold mb-6 text-gray-800">Waste Report</h1>
+        </div>
+    )
 }
