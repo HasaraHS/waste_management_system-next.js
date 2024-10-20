5a2c5666027eae1373ba0e7f2efc3ba28b62df84
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-10-20 15:48:01 +0530

    commit

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -58,7 +58,7 @@ export default function ReportPage() {
         setSearchBox(ref)
     }, []);
 
-    const onPlacedChange = () => {
+    const onPlacedChanged= () => {
         if(SearchBox){
             const places = SearchBox.getPlaces();
             if(places && places.length > 0 ) {
@@ -274,31 +274,80 @@ export default function ReportPage() {
             </div>
           )}
 
-          <Button type="button" onClick={handleVerify} 
-          className="w-full mb-8 bg-gray-600 hover:bg-gray-700 text-white py-3 text-lg transition-colors duration-300"
-          disabled={!file || verificationStatus === 'verifying'}
+          <Button
+            type="button"
+            onClick={handleVerify}
+            className="w-full mb-8 bg-gray-600 hover:bg-gray-700 text-white py-3 text-lg transition-colors duration-300"
+            disabled={!file || verificationStatus === "verifying"}
           >
-            {verificationStatus === 'verifying' ? (
-                <>
-                <Loader className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"/>
+            {verificationStatus === "verifying" ? (
+              <>
+                <Loader className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" />
                 Verifying...
-                </>
-            )  : 'Verify waste'}
+              </>
+            ) : (
+              "Verify waste"
+            )}
           </Button>
 
-          {verificationStatus === 'sucess' && verificationResult && (
+          {verificationStatus === "sucess" && verificationResult && (
             <div className="bg-green-50 border-l-4 border-green-400 p-4 mb-8 rounded-r-xl">
-                <div className="felx ietms-center">
-                    <CheckCircle className="h-6 w-6 text-green-400 mr-3"/>
-                </div>
-                <h3 className="text-lg font-medium text-green-800">Veridication Successful</h3>
-                <div className="mt-2 text-sm text-green-700">
-                    <p>Waste Type : {verificationResult.wasteType}</p>
-                    <p>Quantity : {verificationResult.quantity}</p>
-                    <p>Confidence : {(verificationResult.confidence * 100).toFixed(2)}%</p>
-                </div>
+              <div className="felx ietms-center">
+                <CheckCircle className="h-6 w-6 text-green-400 mr-3" />
+              </div>
+              <h3 className="text-lg font-medium text-green-800">
+                Veridication Successful
+              </h3>
+              <div className="mt-2 text-sm text-green-700">
+                <p>Waste Type : {verificationResult.wasteType}</p>
+                <p>Quantity : {verificationResult.quantity}</p>
+                <p>
+                  Confidence :{" "}
+                  {(verificationResult.confidence * 100).toFixed(2)}%
+                </p>
+              </div>
             </div>
           )}
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+            <div>
+              <label
+                htmlFor="location"
+                className="block text-m font-medium text-gray-700 mb-1"
+              >
+                Location
+              </label>
+              {isLoaded ? (
+                <StandaloneSearchBox
+                  onLoad={onLoad}
+                  onPlacesChanged={onPlacedChanged}
+                >
+                  <input
+                    type="text"
+                    id="location"
+                    name="location"
+                    value={newReport.location}
+                    onChange={handleInputChange}
+                    required
+                    className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300"
+                    placeholder="Enter waste location"
+                  />
+                </StandaloneSearchBox>
+              ) : (
+                <input
+                  type="text"
+                  id="location"
+                  name="location"
+                  value={newReport.location}
+                  onChange={handleInputChange}
+                  required
+                  className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300"
+                  placeholder="Enter waste location"
+                />
+              )}
+            </div>
+            
+          </div>
         </form>
       </div>
     );
